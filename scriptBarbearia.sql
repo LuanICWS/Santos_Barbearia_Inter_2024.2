c451457a263d4bea387fcd14e6db275e857bdea4
jsrbrt <robmail.fds@gmail.com>
2024-10-16 10:27:36 -0300

    update agendamento

--- scriptBarbearia.sql
+++ scriptBarbearia.sql
@@ -8,18 +8,37 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema barbearia_santos
+-- Schema y4l7y7v3pdk47h8m
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema barbearia_santos
+-- Schema y4l7y7v3pdk47h8m
 -- -----------------------------------------------------
--- CREATE SCHEMA IF NOT EXISTS `barbearia_santos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
--- Esse é o nome do banco 'y4l7y7v3pdk47h8m' foi automático do deploy
+CREATE SCHEMA IF NOT EXISTS `y4l7y7v3pdk47h8m` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `y4l7y7v3pdk47h8m` ;
 
 -- -----------------------------------------------------
--- Table `barbearia_santos`.`cadastro`
+-- Table `y4l7y7v3pdk47h8m`.`agendamentos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`agendamentos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `usuario_nome_completo` VARCHAR(100) NOT NULL,
+  `usuario_email` VARCHAR(255) NOT NULL,
+  `usuario_telefone` VARCHAR(15) NOT NULL,
+  `data_do_servico` DATE NULL DEFAULT NULL,
+  `hora_do_servico` TIME NULL DEFAULT NULL,
+  `servicos` TEXT NULL DEFAULT NULL,
+  `data_criacao` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `ultima_mensagem` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `y4l7y7v3pdk47h8m`.`cadastro`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`cadastro` (
   `usuario_nome_completo` VARCHAR(100) NOT NULL,
@@ -33,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`cadastro` (
   `usuario_termos_obrigatorios` TINYINT(1) NOT NULL,
   `usuario_incricao_newsletter` TINYINT(1) NULL DEFAULT NULL,
   PRIMARY KEY (`usuario_id`),
-  UNIQUE INDEX `cpf` (`usuario_cpf` ASC),
-  UNIQUE INDEX `email` (`usuario_email` ASC))
-  ENGINE = InnoDB
-AUTO_INCREMENT = 5
+  UNIQUE INDEX `cpf` (`usuario_cpf` ASC) VISIBLE,
+  UNIQUE INDEX `email` (`usuario_email` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `barbearia_santos`.`login`
+-- Table `y4l7y7v3pdk47h8m`.`login`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`login` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -57,35 +76,27 @@ CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`login` (
   `usuario_termos_obrigatorios` TINYINT(1) NOT NULL,
   `usuario_incricao_newsletter` TINYINT(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `usuario_email` (`usuario_email` ASC))
+  UNIQUE INDEX `usuario_email` (`usuario_email` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`agendamentos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `usuario_nome_completo` VARCHAR(100) NOT NULL,
-  `usuario_email` VARCHAR(255) NOT NULL,
-  `usuario_telefone` VARCHAR(15) NOT NULL,
-  `data_do_servico` DATE,
-  `hora_do_servico` TIME,
-  `servicos` TEXT,
-  `data_criacao` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-);
+-- -----------------------------------------------------
+-- Table `y4l7y7v3pdk47h8m`.`servicos`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `y4l7y7v3pdk47h8m`.`servicos` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nome_servico` VARCHAR(100) NOT NULL,
-    `descricao` TEXT NULL,
-    `preco` DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `nome_servico` (`nome_servico` ASC)
-);
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome_servico` VARCHAR(100) NOT NULL,
+  `descricao` TEXT NULL DEFAULT NULL,
+  `preco` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nome_servico` (`nome_servico` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-SELECT * FROM cadastro;
-SELECT * FROM agendamentos;
