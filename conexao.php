eccaac11dd2224bbc39a45309389f7f4cc10d97d
Lucas Costa <lg1352439@gmail.com>
2024-10-17 16:10:40 -0300

    criptografando as credenciais

--- conexao.php
+++ conexao.php
@@ -1,13 +1,18 @@
 <?php
-$servername = "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "jdocjo0qr6n7fswm";
-$password = "ot4rv2hrpywqbwvp";
-$dbname = "y4l7y7v3pdk47h8m";
-// Criar conexão
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 ?>
+
